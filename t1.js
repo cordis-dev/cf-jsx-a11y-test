5600c5122b3150956119caab94e9e6dd3092da22
cordis-dev <cordis.dev@gmail.com>
2017-10-23 09:46:32 +0300

    Update t1.js

--- t1.js
+++ t1.js
@@ -1,7 +1,8 @@
+<html>
 <span role="img" aria-label="Snowman">&#9731;</span>
 <span role="img" aria-label="Panda">🐼</span>
 <span role="img" aria-labelledby="panda1">🐼</span> 
 
 <span>🐼</span>
 <i role="img" aria-label="Panda">🐼</i>
-
+</html>
