beaf2513f531b4f2e5644e059bedfba5402b32c4
cordis-dev <cordis.dev@gmail.com>
2017-10-23 09:59:18 +0300

    Update t1.js

--- t1.js
+++ t1.js
@@ -1,8 +1,6 @@
 return (
-           <div>
+           <html>
              <span role="img" aria-label="Snowman">&#9731;</span>
              <i role="img" aria-label="Panda">🐼</i>
-           </div>
+           </html>
        )
-
-<html></html>
